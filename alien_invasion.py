6c96c791b5e755fa7b1637afa289b83f5e74f928
Jesica-akeyo <jessica.akeyo@gmail.com>
2023-03-13 15:50:35 +0300

    refactor check event methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,20 +32,30 @@ class AlienInvasion:
         """Respond to key presses and mouse events"""
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        #Move the ship to the right
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        #Move the ship to the left
-                        self.ship.moving_left = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+                    
+    def _check_keydown_events(self, event):
+        """Respond to key presses"""
+        if event.key == pygame.K_RIGHT:
+            #Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            #Move the ship to the left
+            self.ship.moving_left = True
+        #exit game easily    
+        elif event.key == pygame.K_q:
+            sys.exit()
+    def _check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
          """Update images on the screen, and flip to the new screen"""
