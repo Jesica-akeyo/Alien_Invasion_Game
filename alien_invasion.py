bb0803ba39cb61bb47f4db1fe033930df1a09042
Jesica-akeyo <jessica.akeyo@gmail.com>
2023-03-13 15:33:31 +0300

    moving the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,7 +24,8 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            self._check_events()            
+            self._check_events()
+            self.ship.update()            
             self._update_screen()
             
     def _check_events(self):#helper method
@@ -33,6 +34,18 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        #Move the ship to the right
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        #Move the ship to the left
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
     def _update_screen(self):
          """Update images on the screen, and flip to the new screen"""
