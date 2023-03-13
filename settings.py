d7e8f0d30ceefd3d6d0ac7c6d1d05a15bd5de257
Jesica-akeyo <jessica.akeyo@gmail.com>
2023-03-13 13:20:04 +0300

    starting the game

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """ A class to tore all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (230,230,230)
