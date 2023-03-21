71143fb7a6723c0d8d2680e5d945009b766a9f0c
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2023-03-21 23:36:07 +0300

    keras metadata

--- game_functions.py
+++ game_functions.py
@@ -141,10 +141,10 @@ def check_bullet_alien_collision(ai_settings, screen, stats, sb, ship, aliens, b
         sb.prep_level()
         
         create_fleet(ai_settings, screen, ship, aliens)
-    elif len(aliens) == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20 or 21 or 22 or 23 or 24 :
-            stats_data = stats.score, stats.level
-            np.save('stats_data.npy', stats_data)
-            np.save('stats_data', stats_data)     
+#    elif len(aliens) == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20 or 21 or 22 or 23 or 24 :
+#            stats_data = stats.score, stats.level
+#            np.save('stats_data.npy', stats_data)
+#            np.save('stats_data', stats_data)
     
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row."""
